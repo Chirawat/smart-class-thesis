5bdaf07b6f471dd23791d46ed6572a96aa86a4df
Chirawat <chirawat.not@gmail.com>
2016-09-30 16:42:26 +0700

    add ignore files

---
+++ temp/frontend/config/main-local.php
@@ -0,0 +1,24 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'm6YKyAPyNy5NplQa-g5Y4lkt8IQ6dmo2',
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
